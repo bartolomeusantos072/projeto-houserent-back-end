8777f48b66357d948d95e0a63eea177a36fcbff8
Uenderss <meajudabarto@gmail.com>
2022-10-06 20:35:43 -0300

    fix: add service supplier factory

--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -1,6 +1,6 @@
-import { User,Service } from "@prisma/client";
+import { User,Service, Driver } from "@prisma/client";
 
 
 export type CreateUserData = Omit<User,"id">
 export type CreateService = Omit<Service,"id">
-
+export type CreateDriver = Omit<Driver, "id">
---
+++ tests/factories/serviceFactory.ts
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker';
+import { prisma } from "../../src/config/database";
+import { CreateService } from '../../src/utils/typeUtils';
+
+function createServiceSupplierInfo(){
+    return{
+        service:faker.name.jobArea(),
+        photos:{
+            create:[
+                {
+                    name:faker.image.imageUrl(),
+                },
+                {
+                    name:faker.image.imageUrl(),
+                },
+                {
+                    name:faker.image.imageUrl(),
+                },
+                {
+                    name:faker.image.imageUrl(),
+                },
+                ]
+        }
+    }
+}
+
+
+async function createServiceSupplier(createServiceSupplierInfo: CreateService,userId: number ){
+    const service = await prisma.service.create({
+       data:{...createServiceSupplierInfo,userId}
+    });
+
+    return service;
+}
+
+const serviceFactory={
+    createServiceSupplier,
+    createServiceSupplierInfo
+}
+
+export default serviceFactory;
